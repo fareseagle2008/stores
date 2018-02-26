4f949119f5241155c042fb6912e4c273c7c52f10
Fares AbuAmin <fareseagle2012@gmail.com>
2018-02-26 22:24:37 +0200

    flask and views

---
+++ forums/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+@app.route("/index")
+def home():
+	return render_template(	"index.html")
+
+@app.route("/SayHello/<name>")
+def say_hello(name):
+	return f"Hello {name}"
+		
+app.run()
