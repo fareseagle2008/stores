aa1cf71bc6c5e2d24a8bff91f53642c0ffd39428
Fares AbuAmin <fareseagle2012@gmail.com>
2018-02-28 15:56:49 +0200

    jinja workshop

--- forums/app.py
+++ forums/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
+import models
+import stores
+
 app = Flask(__name__)
+post_store = stores.PostStore()
+post_store.add(models.Post("Life", "Life is alawys great"))
+post_store.add(models.Post("Sunshine", "Sunshine is amazing"))
 @app.route("/")
 @app.route("/index")
 def home():
-	return render_template(	"index.html")
-
-@app.route("/SayHello/<name>")
-def say_hello(name):
-	return f"Hello {name}"
+	return render_template(	"index.html", posts = post_store.get_all())
 		
 app.run()
