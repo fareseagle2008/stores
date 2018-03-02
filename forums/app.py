bfd9ec0beef831826b26458a8f1af98f8b890ba3
Fares AbuAmin <fareseagle2012@gmail.com>
2018-03-02 18:07:29 +0200

    Http Post Request workshop

--- forums/app.py
+++ forums/app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 import models
 import stores
 import dummy_data
+from views import *
 
-app = Flask(__name__)
+member_store = stores.MemberStore()
+post_store = stores.PostStore()
 
-@app.route("/")
-@app.route("/index")
-def home():
-	return render_template(	"index.html", posts = dummy_data.post_store.get_all())
-		
-app.run()
+
+if __name__ == "__main__":
+	dummy_data.seed_stores(member_store, post_store)
+	app.run()
--- forums/dummy_data.py
+++ forums/dummy_data.py
@@ -1,6 +1,28 @@
 import stores
 import models
 
-post_store = stores.PostStore()
-post_store.add(models.Post("Life", "Life is alawys great"))
-post_store.add(models.Post("Sunshine", "Sunshine is amazing"))
+dummy_members = [
+	models.Member("Fares AbuAmin", 31),
+	models.Member("Lama Kheir", 30),
+	models.Member("Ali Silan", 28)
+]
+
+dummy_posts =[
+	models.Post("post1", "this is post1", dummy_members[0].id),
+	models.Post("post2", "this is post2", dummy_members[0].id),
+	models.Post("post3", "this is post3", dummy_members[1].id),
+	models.Post("post4", "this is post4", dummy_members[1].id),
+	models.Post("post5", "this is post5", dummy_members[1].id),
+	models.Post("post6", "this is post6", dummy_members[2].id),
+	models.Post("post7", "this is post7", dummy_members[2].id),
+	models.Post("post8", "this is post8", dummy_members[2].id),
+	models.Post("post9", "this is post9", dummy_members[2].id)
+
+]
+
+def seed_stores(member_store, post_store):
+    for member in dummy_members:
+        member_store.add(member)
+
+    for post in dummy_posts:
+        post_store.add(post)
